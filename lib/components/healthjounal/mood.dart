e08e5e8346a013f98d37093364cccaaeeb21f21f
baitlin <caitlinsalinas16@gmail.com>
2025-04-14 21:14:35 -0500

    Update mood.dart

--- lib/components/healthjounal/mood.dart
+++ lib/components/healthjounal/mood.dart
@@ -1,1 +1,16 @@
 
+class Mood {
+  final String imagelib;
+
+  Mood(this.imagelib);
+}
+
+List<Mood> moods = [
+
+  Mood('lib/images/happy1.png'),
+  Mood('lib/images/happy2.png'),
+  Mood('lib/images/happy3.png'),
+  Mood('lib/images/happy4.png'),
+  Mood('lib/images/happy5.png'),
+  
+];
